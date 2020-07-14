f87da011ae24a853da6e93e31c1d98a45d4e6795
NikitaFE <nikromongerredd@gmail.com>
2020-07-14 17:58:37 +0300

    make simple to-do list with React

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Task from './components/Task';
+import TaskInput from './components/TaskInput';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [
+        {id: 0, title: 'Hello, I`m first task!', done: false},
+        {id: 1, title: 'Hello, I`m second task!', done: true},
+        {id: 2, title: 'Hello, I`m third task!', done: false}
+      ]
+    }
+  }
+
+  addTask = task => {
+    this.setState(state => {
+      let { tasks } = state;
+      tasks.push({
+        id: tasks.length !== 0 ? tasks.length : 0,
+        title: task,
+        done: false
+      });
+      return tasks;
+    });
+  }
+
+  doneTask = id => {
+    const index = this.state.tasks.map(task => task.id).indexOf(id);
+    this.setState(state => {
+      let { tasks } = state;
+      tasks[index].done = true;
+      return tasks;
+    })
+  }
+
+  deleteTask = id => {
+    const index = this.state.tasks.map(task => task.id).indexOf(id);
+    this.setState(state => {
+      let { tasks } = state;
+      delete tasks[index];
+      return tasks;
+    })
+  }
+
+  render() {
+    const { tasks } = this.state;
+    const activeTasks = tasks.filter(task => !task.done);
+    const doneTasks = tasks.filter(task => task.done);
+
+    return (
+      <div className="App">
+        <h1 className="top">Active tasks: {activeTasks.length}</h1>
+        {[...activeTasks, ...doneTasks].map(task => (
+          <Task
+            doneTask={() => this.doneTask(task.id)}
+            deleteTask={() => this.deleteTask(task.id)}
+            task={task}
+            key={task.id}
+          />
+        ))}
+        <TaskInput addTask={this.addTask} />
+      </div>
+    );
+  }
+}
+
+export default App;
